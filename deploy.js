bd74ddab8e7486ea09e12ff6bd67c11e685ef355
Peter <ispjme1@gmail.com>
2021-02-20 16:12:06 -0500

    changed deploy

--- deploy.js
+++ deploy.js
@@ -2,8 +2,8 @@ var cmd = require('node-cmd');
 var path, node_ssh, ssh, fs;
 fs = require('fs');
 path = require('path');
-node_ssh = require('node-ssh');
-ssh = new node_ssh();
+const {NodeSSH} = require('node-ssh')
+ssh = new NodeSSH();
 
 // the method that starts the deployment process
 function main() {
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: ' 3.20.234.26',
+      host: '3.20.234.26',
       username: 'ubuntu',
       privateKey: 'hands-key.pem'
     })
